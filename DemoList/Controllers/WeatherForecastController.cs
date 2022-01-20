89ac07a08969bea8ab7ccd461a51b89fb49a38f7
DESKTOP-G0I3496\HuanToCon <huanlb@aisolutions.com.vn>
2022-01-20 16:03:29 +0700

    Add project files.

---
+++ DemoList/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoList.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet( "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("Student")]
+        public List<Student> GetStudents()
+        {
+            _logger.LogInformation("huan");
+            List<Student> list = new List<Student>();
+            for (int i = 0; i < 20; i++)
+            {
+                Student student = new Student();
+                student.StudentName = "huan" + i;
+                student.Age = i;
+                student.Address = "h" + i;
+                list.Add(student);
+            }
+            return list;
+            
+        }
+    }
+}
---
+++ DemoList/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoList
+{
+    public class Student
+    {
+        public string? StudentName { get; set; }
+
+        public int? Age {get; set; }
+
+        public string? Address { get; set; }
+    }
+}
